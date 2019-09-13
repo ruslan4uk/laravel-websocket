bafdb3f9d66fccd0bfe8cd24c822b03d01aa5c5f
Ruslan <rusel91@idz.ru>
2019-09-13 20:41:16 +0400

    test

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -3,12 +3,9 @@
 @section('content')
     @if(Auth::check())
         @verbatim
-
+            <chat></chat>
         @endverbatim
     @else
         <h3 class="text-center">Для просмотра чата необходима авторизация</h3>
-        @verbatim
-            <chat></chat>
-        @endverbatim
     @endif
 @endsection
